ed5105e8157bf2b6cb78a706ed349bf569b319f4
A N A <38155222+resthiana12@users.noreply.github.com>
2019-10-07 23:30:27 +0700

    menambahkan folder project dari latihan video ke6

---
+++ pertemuan6/app/views/templates/footer.php
@@ -0,0 +1,5 @@
+<script src="<?=BASEURL; ?>/js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="<?=BASEURL; ?>/js/popper.min.js"></script>
+<script src="<?=BASEURL; ?>/js/bootstrap.min.js" ></script>
+</body>
+</html>
