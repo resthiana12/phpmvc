5ca2cd563dd08d4314a47fe72abbe4a68f839130
A N A <38155222+resthiana12@users.noreply.github.com>
2019-10-07 22:27:20 +0700

    menambahkan folder project dari latihan video ke5

---
+++ pertemuan5/app/controllers/About.php
@@ -0,0 +1,23 @@
+<?php
+
+class About extends Controller{
+    public function index($nama='Resthiana', $umur=20, $pekerjaan='Programmer')
+    {
+        $data['nama'] = $nama;
+        $data['umur'] = $umur;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['judul'] = 'About';
+
+        $this->view('templates/header', $data);
+        $this -> view('about/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function page()
+    {
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+    }
+}
---
+++ pertemuan5/app/views/about/index.php
@@ -0,0 +1,4 @@
+
+    <h1>About Me</h1>
+    <h4>Halo, nama saya <?=$data['nama']?>, umur saya <?=$data['umur']?> tahun, 
+    saya adalah seorang  <?=$data['pekerjaan']?>. </h4>
